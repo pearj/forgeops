05d926fbad97564436292d94b6dc76d63cb76f54
Joel Pearson <joel.pearson@gmail.com>
2023-07-09 22:56:30 +1000

    Update scripts directly instead of reloading all the configuration

--- bin/pearj/setup-watchman.sh
+++ bin/pearj/setup-watchman.sh
@@ -3,11 +3,14 @@
 SCRIPT_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 CDK_BASE=${SCRIPT_DIR}/../../docker/am/config-profiles/cdk
 SCRIPTING_DIR=config/services/realm/root/scriptingservice/1.0/organizationconfig/default/scriptconfigurations
+AM_ADMIN_PASSWORD=$(kubectl get secret am-env-secrets -o go-template='{{index .data "AM_PASSWORDS_AMADMIN_CLEAR" | base64decode}}') 
+AM_URL=$(kubectl get configmap platform-config -o go-template='{{index .data "AM_URL"}}') 
 
 watchman watch-del-all
 
 watchman watch ${CDK_BASE}/${SCRIPTING_DIR}
 
-watchman -- trigger ${CDK_BASE}/${SCRIPTING_DIR} deployamscript '*.groovy' -- ${SCRIPT_DIR}/reload-scripts.sh
+# watchman -- trigger ${CDK_BASE}/${SCRIPTING_DIR} deployamscript '*.groovy' -- ${SCRIPT_DIR}/reload-scripts.sh
+watchman -- trigger ${CDK_BASE}/${SCRIPTING_DIR} deployamscript '*.groovy' -- ${SCRIPT_DIR}/update-scripts.py --base-dir ${CDK_BASE}/${SCRIPTING_DIR} --username amadmin --password ${AM_ADMIN_PASSWORD} --base-am-url ${AM_URL}
 
 exec tail -F $(watchman get-log | jq -r .log)
---
+++ bin/pearj/update-scripts.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+
+import requests
+import base64
+import argparse
+import os
+import re
+import json
+from collections import namedtuple
+import urllib3
+
+ScriptNode = namedtuple('ScriptNode', ['id', 'groovy_path', 'json_path', 'json'])
+
+# We don't want the TLS warnings, as we likely don't have proper TLS certificates to begin with
+urllib3.disable_warnings()
+
+class ScriptReload:
+    def __init__(self, base_dir: str, base_am_url: str, username: str, password: str) -> None:
+        self.base_dir = base_dir
+        self.base_am_url = base_am_url
+        self.session = requests.Session()
+
+        self.authenticate_am(username=username, password=password)
+
+        pass
+
+    uidRegex = r"^ou=([\w-]+),"
+
+    def find_json_script_node_file(self, groovy_filename) -> ScriptNode:
+        print(f"looking for script node for: {groovy_filename}")
+        for root, dirs, fnames in os.walk(self.base_dir):
+            for fname in fnames:
+                if fname.endswith(".json"):
+                    # print(f"Searching {fname}")
+                    path = os.path.join(root, fname)
+                    with open(path) as jsonfp:
+                        jsonfile = json.load(fp=jsonfp)
+                    
+                    try:
+                        inline_file = (jsonfile["data"]["script"]["$base64:encode"]["$inline"])
+                        if inline_file == groovy_filename:
+                            print(f"Found a match in {fname}")
+                            matches = re.search(self.uidRegex, jsonfile["metadata"]["uid"])
+
+                            return ScriptNode(id=matches.group(1), groovy_path=os.path.join(self.base_dir, groovy_filename), json_path=path, json=jsonfile)
+                        
+                    except KeyError:
+                        pass
+        print(f"Warning: Didn't find script node for {groovy_filename}")
+
+    def prepare_payload(self, script_node:ScriptNode):
+        payload = script_node.json["data"]
+        payload["_id"] = script_node.id
+
+        with open(script_node.groovy_path, 'rb') as groovy_file:
+            file_bytes = bytearray(groovy_file.read())
+            payload["script"] = base64.b64encode(file_bytes).decode("ascii")
+        
+        return payload
+
+    def authenticate_am(self, username: str, password: str):
+        headers = {
+            "X-OpenAM-Username": username,
+            "X-OpenAM-Password": password,
+            "Accept-API-Version": "protocol=2.0,resource=1.0"
+        }
+
+        self.session.post(f"{self.base_am_url}/json/realms/root/authenticate", headers=headers, verify=False)
+
+    def update_script(self, data, script_node: ScriptNode):
+        headers = {
+            "Accept-API-Version": "protocol=2.0,resource=1.0",
+            "Content-type": "application/json"
+        }
+
+        res = self.session.put(f"{self.base_am_url}/json/scripts/{script_node.id}", headers=headers, verify=False, data=json.dumps(data))
+
+        print(res)
+
+        print(f"Updated Script {script_node.groovy_path}")
+
+def main():
+    parser = argparse.ArgumentParser(description='Reload changed AM scripts')
+    parser.add_argument('-b', '--base-dir', required=True)
+    parser.add_argument('-u', '--username', required=True)
+    parser.add_argument('-p', '--password', required=True)
+    parser.add_argument('-a', '--base-am-url', required=True)
+    parser.add_argument('changed_files', nargs='*')
+
+    args = parser.parse_args()
+
+    script_reload = ScriptReload(base_dir=args.base_dir, base_am_url=args.base_am_url, username=args.username, password=args.password)
+    for file in args.changed_files:
+        script_node = script_reload.find_json_script_node_file(groovy_filename=file)
+
+        if script_node:
+            data = script_reload.prepare_payload(script_node=script_node)
+            script_reload.update_script(data=data, script_node=script_node)
+
+main()
